36b22c3158c46bab17259d90f83915dd1a9ee2e0
Jetaime Alexandria <rondellivey03@gmail.com>
2024-02-25 17:37:02 -0500

    Added a 404 page.

---
+++ 404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+  <title>Monohead</title>
+</head>
+
+<body>
+ <h2><i class="fa-solid fa-magnifying-glass"></i> Page Not Found</h2>
+ <p>Well isn't that just great? Maybe try going back to the <a href="/">home page</a>?</p>
+</body>
+</html>
